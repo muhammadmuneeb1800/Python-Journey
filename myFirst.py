49861d00c82e4e4999013d2d74a35227ec7b3018
Muhammad Muneeb <nadeemmk1900@gmail.com>
2025-03-25 16:43:43 +0500

    start the python journey

---
+++ myFirst.py
@@ -0,0 +1,87 @@
+# import random
+
+# x = "this is first variable in the"
+# print(x)
+
+# # this is commented code
+# y = 10
+# print(y)
+
+# if 5 > 2:
+#     print("5 is greater than 2")
+# if 5 < 3:
+#     print("3 is greater than 3")
+
+# print("Muhammad Muneeb is a website developer,")
+
+# print("This is random number", random.randrange(1, 10))
+
+
+# name = input("Enter the name ")
+# print("the length of name is", len(name))
+
+# dolor = "$this is $ and it $price$ is $$$"
+
+# print(dolor.count("$"))
+
+
+# light = "green"
+
+# if light == "red":
+#     print("Stop")
+# elif light == "yellow":
+#     print("Warning")
+# elif(light == "green"):
+#     print("Go")
+# else:
+#     print("Broken")
+
+# age = 10
+# if age >= 18:
+#     print("you are eligible")
+# else:
+#     print("you are not eligible")
+
+
+# marks = int(input("Enter your marks: "))
+
+# if marks >= 90:
+#     print("Grade: A")
+# elif marks >= 80:
+#     print("Grade: B")
+# elif marks >= 70:
+#     print("Grade: C")
+# elif marks >= 60:
+#     print("Grade: D")
+# else:
+#     print("Grade: F")
+
+
+# number = int(input("Number: "))
+
+# if number % 2 == 0:
+#     print("Even")
+# else:
+#     print("Odd")
+
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# c = int(input("Enter third number: "))
+
+# if a >= b and a >= c:
+#     print("The first largest number", a)
+# elif b >= c and b >= a:
+#     print("The second largest number", b)
+# elif c >= a and c >= b:
+#     print("The third largest number", c)
+# else:
+#     print("All numbers are equal")
+
+
+a = int(input("Enter number: "))
+
+if a % 5 == 0:
+    print("The number is divisible by 5")
+else:
+    print("The number is not divisible by 5")
